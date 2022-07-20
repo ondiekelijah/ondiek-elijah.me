031a20a6c636a4737624f730a5664830f0346cbe
Dev-Elie <eochieng9448@gmail.com>
2022-07-20 12:59:44 +0300

    Updated site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,9 +4,9 @@ const siteMetadata = {
   headerTitle: 'Dev Elie',
   description: "Ondiek Elijah's personal blog",
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/Dev-Elie/ondiek-elijah',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/ondiek.png',
   socialBanner: '/static/images/twitter-card.png',
