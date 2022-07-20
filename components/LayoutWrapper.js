e8c22e4ed83755c3b45dfdc1c7ba3657a74d79f6
Dev-Elie <eochieng9448@gmail.com>
2022-07-20 14:58:10 +0300

    feat: added giscus

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
-import siteMetadata from '@/data/siteMetadata'
+import { useState, useEffect, useRef } from 'react'
+
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
