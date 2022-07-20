2c1782466ff809e91126fa69e425ed6266a78025
Dev-Elie <eochieng9448@gmail.com>
2022-07-20 19:06:35 +0300

    update: added Google analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,7 @@ const siteMetadata = {
     plausibleDataDomain: 'https://www.ondiek-elijah.me/', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID, // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
