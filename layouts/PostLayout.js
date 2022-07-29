8984ea656479655bac2db7c0e25f1dc4c151e7b8
Dev-Elie <eochieng9448@gmail.com>
2022-07-29 13:21:22 +0300

    Updated npm packages

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,6 +6,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
+import SocialIcon from '@/components/social-icons'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/edit/master/data/blog/${fileName}`
