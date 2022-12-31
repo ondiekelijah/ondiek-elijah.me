6d998464c674225ac8acfadfa271dfa7b1bc4ff6
Dev-Elie <eochieng9448@gmail.com>
2022-12-31 14:34:41 +0300

    chore: modified eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,16 @@ module.exports = {
   },
   extends: ['plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      0,
+      {
+        endOfLine: "auto",
+      },
+    ],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'react/no-unescaped-entities': 0,
   },
 }
+
