9e9f6d2dfd70ff6855523de5151b322d90bca253
ondiekelijah <eochieng9448@gmail.com>
2023-02-24 16:30:22 +0300

    chore: updated experience

--- data/experienceData.js
+++ data/experienceData.js
@@ -2,12 +2,32 @@ const experienceData = [
   {
     title: 'Software Engineer',
     company: 'Safaricom',
-    location: 'Remote, Kenya',
+    location: 'Hybrid, Kenya',
     range: 'November 2022 - Present',
     url: 'https://www.safaricom.co.ke/',
     text1: 'Working with designers to translate visual designs into functional interfaces.',
     text2: 'Designing and implementing user interfaces for web applications using React, Next.js and MUI.',
     text3: 'Collaborating with backend developers to integrate frontend interfaces with server-side logic & testing and debugging frontend code.'
+  },
+  {
+    title: 'Author',
+    company: 'Educative Inc',
+    location: 'Remote, Kenya',
+    range: 'February 2023 - Present',
+    url: 'https://www.educative.io/',
+    text1: 'Creating high-quality educational content that is designed to help learners improve their skills and knowledge in various fields.',
+    text2: 'Contributing to the development of comprehensive courses that are tailored to meet the needs of learners at different levels, from beginners to advanced learners.',
+    text3: 'Through my work at Educative.io, I am committed to empowering individuals and organizations with the knowledge and skills they need to succeed in a rapidly changing world.'
+  },
+  {
+    title: 'Technical Writer',
+    company: 'Draft.dev',
+    location: 'Remote, Kenya',
+    range: 'February 2023 - Present',
+    url: 'https://draft.dev/',
+    text1: 'Creating articles that focus on automation testing.',
+    text2: 'Researching and writing high-quality content that provides valuable insights into the latest trends and best practices in automation testing.',
+    text3: 'Working closely with subject matter experts and other stakeholders to ensure my articles are accurate, informative, and engaging.',
   },
   {
     title: 'Technical Writer',
