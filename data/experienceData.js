57a0c1c4540766152db18cafecaf72064f34517a
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2024-05-21 01:33:30 +0300

    feat: add project and reorder existing

--- data/experienceData.js
+++ data/experienceData.js
@@ -5,11 +5,12 @@ const experienceData = [
     location: 'Hybrid, Kenya',
     range: 'November 2022 - Present',
     url: 'https://www.safaricom.co.ke/',
-    text1: 'Working with designers to translate visual designs into functional interfaces.',
+    text1:
+      'Working with designers to translate visual designs into responsive, functional, and accessible user interfaces.',
     text2:
-      'Designing and implementing user interfaces for web applications using React, Next.js and MUI.',
+      'Developing dynamic web applications using Next.js (JavaScript/TypeScript), MUI (with TailwindCSS), and GraphQL (Apollo Server), ensuring performance optimization and cross-browser compatibility.',
     text3:
-      'Collaborating with backend developers to integrate frontend interfaces with server-side logic & testing and debugging frontend code.',
+      'Ensuring code quality by writing robust unit and integration tests using Jest, React Testing Library, and Cypress, along with performance tuning for optimal user experiences.',
   },
   {
     title: 'Software Engineer Intern',
@@ -17,11 +18,12 @@ const experienceData = [
     location: 'Remote, Kenya',
     range: 'May 2022 - July 2022',
     url: 'https://itesyl.com/',
-    text1: 'Backend Development and Support: Supported and enhanced the Real Estate FastAPI backend, integrating new features designed to streamline operations for caretakers and enhance overall functionality.',
+    text1:
+      'Backend Development and Support: Supported and enhanced the Real Estate FastAPI backend, integrating new features designed to streamline operations for caretakers and enhance overall functionality.',
     text2:
       'WhatsApp Bot and Technology Integration: Overhauled the WhatsApp bot for Kodeec, an advanced business banking solution for the real estate sector, using Google Dialogflow. Ensured robust performance and extended bot capabilities using TypeScript, Node.js, and Express.',
-    text3: 'Web Design and User Experience: Revitalized the company’s existing website and the Kodeec platform, applying cutting-edge design and development practices. This work significantly improved user experience and functionality, tailoring both platforms to meet the specialized needs of real estate professionals.',
-    
+    text3:
+      'Web Design and User Experience: Revitalized the company’s existing website and the Kodeec platform, applying cutting-edge design and development practices. This work significantly improved user experience and functionality, tailoring both platforms to meet the specialized needs of real estate professionals.',
   },
 ]
 
